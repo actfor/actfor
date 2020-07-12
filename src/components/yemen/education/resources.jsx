56a10c107ce61cd7998b9c13f0ffcbc331dae6d9
Mo Khatami <mokhatami113@gmail.com>
2020-07-11 22:00:19 -0400

    "styled on the card in resources"

--- src/components/yemen/education/resources.jsx
+++ src/components/yemen/education/resources.jsx
@@ -8,16 +8,18 @@ export default class Resources extends Component {
   render() {
     return (
       <div>
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '25rem' }}>
           <Card.Img variant="top" src={IRC} />
           <Card.Body>
-            <Card.Title>The Health Crisis in Yemen</Card.Title>
+            <Card.Title>The Yemeni Health Crisis</Card.Title>
             <Card.Text>
               Despite being a completely treatable disease, thousands of people have died from the outbreak of
               cholera unleashed in Yemen in 2017. The health care system has been decimated by years of unrelenting
               war. Supplies and medical care are scarce as is the access to drinking water and sanitation.
             </Card.Text>
-            <Button variant="primary">Visit Site</Button>
+            <Button href='https://www.icrc.org/en/where-we-work/middle-east/yemen/health-crisis-yemen' variant="primary" target='_blank'>
+              Visit Site
+            </Button>
           </Card.Body>
         </Card>
       </div>
