5c3d4de3998c5d44df67175b983d79434a55a1aa
Aman Ibrahim <amanmibra@gmail.com>
2020-07-18 12:47:48 -0400

    Deploy Google Analytics for staging

--- src/ActFor.jsx
+++ src/ActFor.jsx
@@ -7,18 +7,31 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+
+// Google Analytics
+import ReactGA from 'react-ga';
 
 // containers
 import About from './containers/About';
 import Home from './containers/Home';
 import Yemen from './containers/Yemen';
 
+// Initialize google analytics page view tracking
+const trackingId = "UA-73887375-2"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 class ActFor extends Component {
 
   render() {
     return (
       <div style={{ margin: '24px' }} >
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route exact path="/">
               <Home />
