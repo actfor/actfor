777eb004445e6d5f85f0797cdea36d455d461b65
Aman Ibrahim <amanmibra@gmail.com>
2020-07-26 19:13:20 -0400

    Added Donate descriptions

--- src/components/yemen/donate/index.jsx
+++ src/components/yemen/donate/index.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import {
   Button,
   Card,
+  Container,
   Divider,
   Image,
 } from 'semantic-ui-react';
@@ -22,33 +23,38 @@ class Donate extends Component {
       {
         header: 'Mona Relief',
         img: mona,
-        link: 'https://www.monareliefye.org/'
+        link: 'https://www.monareliefye.org/',
+        description: 'Yemen Organization for Humanitarian Relief and Development (Mona), is a national independent, non-governmental and non-profitable organization based in Yemen. The organization activities are mainly focused in the field of humanitarian relief and development by motivating and organizing volunteer, charitable and humanitarian work.'
       },
       {
         header: 'Mwatana for Human Rights',
         img: mwatana,
         link: 'https://mwatana.org/en/',
+        description: 'Mwatana is an independent Yemeni organization that advocates for human rights through the verification and documentation of violations, provision of legal support to victims, lobbying, as well as awareness raising and capacity building.'
       },
       {
         header: 'Oxfam',
         img: oxfam,
         link: 'https://www.oxfamamerica.org/explore/countries/yemen/',
+        description: 'Oxfam is a global organization working to end the injustice of poverty. We help people build better futures for themselves, hold the powerful accountable, and save lives in disasters.'
       },
       {
         header: 'UNICEF Yemen',
         img: unicef,
         link: 'https://www.unicef.org/yemen/',
+        description: 'UNICEF established its presence in Yemen in the 1970s to respond to the urgent needs of children in the poorest country in the Middle East.'
       },
       {
         header: 'Zahra Trust',
         img: zahra,
         link: 'https://zahratrust.org/',
+        description: 'The Zahra Trust was founded as a compassionate response to witnessing poverty first hand in the Middle East. It was established as a unique transparent organization that not only assists with the distribution of aid but also raises awareness of development, humanitarian and spiritual issues.'
       }
     ];
 
 
     return (
-      <div>
+      <Container>
         <Card.Group centered stackable>
           {cards.map(card => (
             <Card color="secondary">
@@ -56,6 +62,9 @@ class Donate extends Component {
               <Card.Content>
                 <Card.Header>{card.header}</Card.Header>
                 <Divider />
+                <Card.Description>{card.description}</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
                 <Button
                   primary
                   as='a' target="_blank"
@@ -63,12 +72,12 @@ class Donate extends Component {
                   content='Visit Site'
                   icon='external alternate'
                   labelPosition='left'
-                ></Button>
+                  />
               </Card.Content>
             </Card>
           ))}
         </Card.Group>
-      </div>
+      </Container>
     );
   }
 }
