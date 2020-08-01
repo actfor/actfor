291cad44545abb6676336e41a67f39689b4c2978
Ahmad Khan <aak43@duke.edu>
2020-08-01 18:31:26 -0500

    added Uyghur organization logos to cards

--- src/components/uyghur/donate/index.jsx
+++ src/components/uyghur/donate/index.jsx
@@ -10,10 +10,10 @@ import {
 } from 'semantic-ui-react';
 
 // images
-import * as mona from './../../../images/mona.png';
-import * as mwatana from './../../../images/mwatana.jpg';
-import * as oxfam from './../../../images/oxfam.png';
-import * as unicef from './../../../images/unicef.png';
+import * as uyghurCampaign from './../../../images/uyghurCampaignLogo.jpg';
+import * as uyghurAssociation from './../../../images/uyghurAssociationLogo.jpg';
+import * as uyghurJustice from './../../../images/uyghurJusticeLogo.png';
+import * as uyghurRights from './../../../images/uyghurRightsLogo.jpg';
 import * as zahra from './../../../images/zahra.jpg';
 
 class UyghurDonate extends Component {
@@ -22,31 +22,25 @@ class UyghurDonate extends Component {
     const cards = [
       {
         header: 'Mona Relief',
-        img: 'https://www.justiceforall.org/wp-content/uploads/2019/09/Artboard-7-8-1.png',
+        img: uyghurJustice,
         link: 'https://www.monareliefye.org/',
         description: 'Yemen Organization for Humanitarian Relief and Development (Mona), is a national independent, non-governmental and non-profitable organization based in Yemen. The organization activities are mainly focused in the field of humanitarian relief and development by motivating and organizing volunteer, charitable and humanitarian work.'
       },
       {
         header: 'Mwatana for Human Rights',
-        img: mwatana,
+        img: uyghurRights,
         link: 'https://mwatana.org/en/',
         description: 'Mwatana is an independent Yemeni organization that advocates for human rights through the verification and documentation of violations, provision of legal support to victims, lobbying, as well as awareness raising and capacity building.'
       },
       {
         header: 'Oxfam',
-        img: oxfam,
+        img: uyghurCampaign,
         link: 'https://www.oxfamamerica.org/explore/countries/yemen/',
         description: 'Oxfam is a global organization working to end the injustice of poverty. We help people build better futures for themselves, hold the powerful accountable, and save lives in disasters.'
       },
       {
-        header: 'UNICEF Yemen',
-        img: unicef,
-        link: 'https://www.unicef.org/yemen/',
-        description: 'UNICEF established its presence in Yemen in the 1970s to respond to the urgent needs of children in the poorest country in the Middle East.'
-      },
-      {
         header: 'Zahra Trust',
-        img: zahra,
+        img: uyghurAssociation,
         link: 'https://zahratrust.org/',
         description: 'The Zahra Trust was founded as a compassionate response to witnessing poverty first hand in the Middle East. It was established as a unique transparent organization that not only assists with the distribution of aid but also raises awareness of development, humanitarian and spiritual issues.'
       }
