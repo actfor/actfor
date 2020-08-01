ec5d8e56f20b3b0f044b178c33eebf96f4d15298
Ahmad Khan <aak43@duke.edu>
2020-08-01 18:42:31 -0500

    updated Uyghur donate card links

--- src/components/uyghur/donate/index.jsx
+++ src/components/uyghur/donate/index.jsx
@@ -23,25 +23,25 @@ class UyghurDonate extends Component {
       {
         header: 'Justice For All: Uyghur Project',
         img: uyghurJustice,
-        link: 'https://www.monareliefye.org/',
+        link: 'https://www.justiceforall.org/save-uighur/',
         description: 'Yemen Organization for Humanitarian Relief and Development (Mona), is a national independent, non-governmental and non-profitable organization based in Yemen. The organization activities are mainly focused in the field of humanitarian relief and development by motivating and organizing volunteer, charitable and humanitarian work.'
       },
       {
         header: 'Uyghur Human Rights Project',
         img: uyghurRights,
-        link: 'https://mwatana.org/en/',
+        link: 'https://uhrp.org/',
         description: 'Mwatana is an independent Yemeni organization that advocates for human rights through the verification and documentation of violations, provision of legal support to victims, lobbying, as well as awareness raising and capacity building.'
       },
       {
         header: 'Campaign For Uyghurs',
         img: uyghurCampaign,
-        link: 'https://www.oxfamamerica.org/explore/countries/yemen/',
+        link: 'https://campaignforuyghurs.org/',
         description: 'Oxfam is a global organization working to end the injustice of poverty. We help people build better futures for themselves, hold the powerful accountable, and save lives in disasters.'
       },
       {
         header: 'Uyghur American Association',
         img: uyghurAssociation,
-        link: 'https://zahratrust.org/',
+        link: 'https://uyghuraa.org/',
         description: 'The Zahra Trust was founded as a compassionate response to witnessing poverty first hand in the Middle East. It was established as a unique transparent organization that not only assists with the distribution of aid but also raises awareness of development, humanitarian and spiritual issues.'
       }
     ];
