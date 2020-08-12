d2c1c7cc48ff00bb0c63e5f913ec2101cecc5e01
Aman Ibrahim <amanmibra@gmail.com>
2020-08-12 00:20:34 -0400

    Remove blm for prod

--- src/components/home/HomeDropdown.jsx
+++ src/components/home/HomeDropdown.jsx
@@ -18,11 +18,11 @@ class HomeDropdown extends Component {
 
   render() {
     const options = [
-      {
-        key: 'blm',
-        value: 'blm',
-        text: 'Black Lives Matter',
-      },
+      // {
+      //   key: 'blm',
+      //   value: 'blm',
+      //   text: 'Black Lives Matter',
+      // },
       {
         key: 'yemen',
         value: 'yemen',
@@ -48,21 +48,6 @@ class HomeDropdown extends Component {
       borderRadius: '24px'
     };
 
-    // return (
-    //   <div style={style}>
-    //     <Image centered src={title} size="medium" />
-    //     <br />
-    //     <br />
-    //     <Dropdown
-    //       fluid
-    //       selection
-    //       search
-    //       placeholder='Choose one'
-    //       onChange={this.onChange}
-    //       options={options}
-    //     />
-    //   </div>
-    // );
     return (
       <div style={style}>
         <Dropdown
