70ece18385ef8e256590852aa050cfe89fab1b15
Aman Ibrahim <amanmibra@gmail.com>
2020-08-12 01:06:06 -0400

    Update mail button

--- src/components/uyghur/pressure/brands/BrandsSegment.jsx
+++ src/components/uyghur/pressure/brands/BrandsSegment.jsx
@@ -14,6 +14,7 @@ import reactElementToJSXString from 'react-element-to-jsx-string';
 
 // components
 import MailTo from '../../../common/MailTo';
+import CopyMessage from '../../../common/CopyMessage';
 
 // messages
 import { brandsMess } from '../messages'
@@ -31,10 +32,17 @@ export default class BrandsSegment extends Component {
               <MailTo
                 email={'anfchallenge@anfcorp.com; diversity@anfcorp.com; cr@acer.com; onlineservice@asus.com; bmwgenius@bmwusa.com; calvinkleincustomerservice@support.calvinklein.com; contactus@carters.com; support@res.cisco.com; consumercare@fila.com; custserv@gap.com; info@hm.com; a.gedda@hm.com; sustainability@hp.com; techregshelp@hp.com; support@huawei.com; jagweb1@jaguarlandrover.com; transportservices@alstrom.com; sales@baicintl.com; mbfs.fraudcontrol@daimler.com; outdoor.discovery@llbean.com; dataprotection@lacoste.com; lradvice@jaguarlandrover.com; angelalee@lenovo.com; Emailcac.info@lge.com; newsbureau@mmsa.com; NIGEL.POWELL@NIKE.COM; pr@nintendo.co.uk; services@nokia.com; Tnf.Ukshop@Vfc.Com; media@oculus.com; pids@ca.panasonic.com; RL-PRESS@RALPHLAUREN.COM; denise.pflock@puma.com; corp.comm@sea.samsung.com; David.Weinberg@skechers.com; SCA.Communications@am.sony.com; tommyhelp@tommy.com; ToshibaPR@accesspr.com;  wecare@uniqlo-usa.com; Paulo.Monteiro@vw.com; contact.ww@zara.com;  care@adidas.com; david.caldwell@gm.com; rtt@we-worldwide.com; tellvictoria@victoriassecret.com; press@google.com'}
                 subject={'Regarding your contracts with manufacturers in Xinjiang'}
-                body={reactElementToJSXString(brandsMess(name, location))}
+                // body={reactElementToJSXString(brandsMess(name, location))}
               >
                 <Button primary>Send an email to <br /> the following companies</Button>
               </MailTo>
+        </Segment>
+        <Segment>
+          <CopyMessage
+            id="brandMessage"
+            header="Copy All Brand Emails to Clipboard"
+            message="anfchallenge@anfcorp.com;diversity@anfcorp.com;cr@acer.com;onlineservice@asus.com;bmwgenius@bmwusa.com;calvinkleincustomerservice@support.calvinklein.com;contactus@carters.com;support@res.cisco.com;consumercare@fila.com;custserv@gap.com;info@hm.com;a.gedda@hm.com;sustainability@hp.com;techregshelp@hp.com;support@huawei.com;jagweb1@jaguarlandrover.com;transportservices@alstrom.com;sales@baicintl.com;mbfs.fraudcontrol@daimler.com;outdoor.discovery@llbean.com;dataprotection@lacoste.com;lradvice@jaguarlandrover.com;angelalee@lenovo.com;Emailcac.info@lge.com;newsbureau@mmsa.com;NIGEL.POWELL@NIKE.COM;pr@nintendo.co.uk;services@nokia.com;Tnf.Ukshop@Vfc.Com;media@oculus.com;pids@ca.panasonic.com;RL-PRESS@RALPHLAUREN.COM;denise.pflock@puma.com;corp.comm@sea.samsung.com;David.Weinberg@skechers.com;SCA.Communications@am.sony.com;tommyhelp@tommy.com;ToshibaPR@accesspr.com;wecare@uniqlo-usa.com;Paulo.Monteiro@vw.com;contact.ww@zara.com;care@adidas.com;david.caldwell@gm.com;rtt@we-worldwide.com;tellvictoria@victoriassecret.com;press@google.com"
+          />
         </Segment>
   
         <Segment>
