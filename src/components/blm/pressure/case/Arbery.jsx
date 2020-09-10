e8d0ca5773914865ca314597096ae31087157312
Ahmad Khan <akhanduke@gmail.com>
2020-09-10 17:31:20 -0500

    implemented contact table or Arbery case

--- src/components/blm/pressure/case/Arbery.jsx
+++ src/components/blm/pressure/case/Arbery.jsx
@@ -6,7 +6,8 @@ import {
     Divider,
     Header,
     Segment,
-  } from 'semantic-ui-react';
+    Table
+} from 'semantic-ui-react';
 
 // components
 import StepMessage from '../../../common/StepMessage';
@@ -23,20 +24,41 @@ class Arbery extends Component {
     renderStep3Segment = () => {
 
         return (
-          <Segment placeholder>
-            <Button.Group vertical primary>
-              
-                <Button
-                  as="a"
-                  target="_blank"
-                  href={"mailto:cobbdistrictattorney@cobbcounty.org"}
-                  content={"Cobb County DA: Joyette Holmes"}
-                />
-              
-            </Button.Group>
-          </Segment>
+            <Segment placeholder>
+                <Table celled selectable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Official</Table.HeaderCell>
+                            <Table.HeaderCell>Contact Information</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>Governor Beshear</Table.Cell>
+                            <Table.Cell>Phone: (502) 564-2611 or message online <a href="https://governor.ky.gov/contact/contact-us">here</a></Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>Mayor Greg Fischer</Table.Cell>
+                            <Table.Cell>Phone: (502) 574-2003 or message online <a href="https://louisvilleky.gov/government/mayor-greg-fischer/contact-mayors-office">here</a> </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>Jefferson County Attorney</Table.Cell>
+                            <Table.Cell>Phone: (502) 574-6336 or message online <a href="https://louisvilleky.wufoo.com/forms/contact-the-jefferson-county-attorney/">here</a> </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>Attorney General Cameron</Table.Cell>
+                            <Table.Cell>Phone: (502) 429-7134 or message online <a href="https://ag.ky.gov/Contact-Us/Pages/default.aspx">here</a> </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>Interim LMPD Police Chief Schroeder</Table.Cell>
+                            <Table.Cell>Email here: <a href="mailto:Robert.Schroeder@louisvilleky.gov">Robert.Schroeder@louisvilleky.gov</a></Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+            </Segment>
         )
-      }
+    }
     render() {
 
 
