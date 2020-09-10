91a79c4f01af01427c4bc69aca04bb3fce58a013
Ahmad Khan <akhanduke@gmail.com>
2020-09-10 18:53:37 -0500

    updated Hollowell contact table

--- src/components/blm/pressure/case/Hollowell.jsx
+++ src/components/blm/pressure/case/Hollowell.jsx
@@ -3,16 +3,47 @@ import React, { Component } from 'react';
 // components
 import StepMessage from '../../../common/StepMessage';
 
+// semantic-ui
+import {
+    Segment,
+    Table
+} from 'semantic-ui-react';
+
 // messages
 import { caseHollowell } from './../messages';
 
-// segments
-import {
-    renderOtherSegment,
-    renderStep3Segment
-} from '../congress/renderSegments';
-
 class Hollowell extends Component {
+
+    renderStep3Segment = () => {
+
+        return (
+            <Segment placeholder>
+                <Table celled selectable>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Official</Table.HeaderCell>
+                            <Table.HeaderCell>Contact Information</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>Mayor Steve Adler</Table.Cell>
+                            <Table.Cell>Phone: 512-978-2100 or message online <a href="https://www.austintexas.gov/email/steveadler">here</a></Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>City Manager Spencer Crock</Table.Cell>
+                            <Table.Cell>Phone: 512-974-2220</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>City Council Members</Table.Cell>
+                            <Table.Cell>Phone: (502) 574-6336 or message online <a href="https://www.austintexas.gov/email/all-council-members">here</a> </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+            </Segment>
+        )
+    }
     render() {
         const steps = [
             {
@@ -28,7 +59,7 @@ class Hollowell extends Component {
                 type: 'other',
                 name: 'Step 3',
                 subheader: 'Reach out to Texas officials with your message',
-                segment: renderStep3Segment(),
+                component: this.renderStep3Segment,
             },
 
         ]
