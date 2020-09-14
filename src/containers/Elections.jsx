69349e816dad4c3b1d92a37a7cbda533c7ad3f06
Aman Ibrahim <amanmibra@gmail.com>
2020-09-13 23:07:38 -0400

    Add election sheet

--- src/containers/Elections.jsx
+++ src/containers/Elections.jsx
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 
 // semantic-ui
 import {
+  Button,
   Container,
+  Divider,
   Header,
  } from 'semantic-ui-react';
 
 // components
 import NavBar from './../components/navbar';
 
+const URL = "https://docs.google.com/spreadsheets/d/1Tdqmms5h0AGcDDI0x2xHc7z21fXpH1LUkhb21dXbOIc/edit?usp=sharing"
+
 class Elections extends Component {
   render() {
     return (
       <div>
         <NavBar unscrollable />
-        <Container textAlign="center">
-          <Header as="h1">
-            U.S. Elections 2020
-          </Header>
-        </Container>
+          <div>
+            <Header as="h1">
+              U.S. Elections 2020
+            </Header>
+            <Divider />
+            <iframe
+              height="500px"
+              width="100%"
+              src={URL}
+            />
+            <br />
+            <br />
+            <Button
+              primary
+              as='a' target="_blank"
+              href={URL}
+              content='Visit the Google Sheet'
+              size='big'
+              icon='google drive'
+              labelPosition='left'
+            />
+          </div>
       </div>
     );
   }
