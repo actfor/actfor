49cb40acd8c2e41e8980b79a994dc342134b898c
Aman Ibrahim <amanmibra@gmail.com>
2020-09-21 01:03:00 -0400

    BLM edit

--- src/components/blm/people/cards.js
+++ src/components/blm/people/cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // images
+import * as angela from './images/AngelaDavis.png';
 import * as cullors from './images/PatriseCullors.png';
 import * as garza from './images/AliciaGarza.png';
 import * as mckesson from './images/DerayMckesson.png';
@@ -22,7 +23,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Patrisse Cullors is a co-founder of the Black Lives Matter movement. She advocates the abolition of prisons in Los Angeles as executive director of the L.A. Coalition to End Violence in Jails. The group proposed a legislative commission that would oversee the Los Angeles County Sheriff’s Department with a view to reducing police brutality. Cullors co-founded the Dignity and Power Now, a jail lobbying organization that served to advocate for a national advisory council as well as a board member of the Ella Baker Center for Human Rights, having led a think tank for the 2014 Without Borders Conference. She created the hashtag #BlackLivesMatter and won numerous awards in 2015 for activism such as an NAACP History Maker. 
+        Patrisse Cullors is a co-founder of the Black Lives Matter movement. She advocates for the abolition of prisons in Los Angeles as executive director of the L.A. Coalition to End Violence in Jails. The group proposed a legislative commission that would oversee the Los Angeles County Sheriff’s Department with a goal to reduce police brutality. Cullors co-founded the Dignity and Power Now, a jail lobbying organization that serves to advocate for a national advisory council. She was also a board member of the Ella Baker Center for Human Rights, having led a think tank for the 2014 Without Borders Conference. She created the hashtag #BlackLivesMatter and won numerous awards in 2015 for activism such as an NAACP History Maker. 
         </p>
       </div>
     ),
@@ -33,7 +34,7 @@ const activists = [
     content: (
       <div>
         <p>
-            Alicia Garza is a co-founder of the Black Lives Matter. Garza has also won the Justice Seekers Glamour Awards. She has helped coordinate the very first 2002 Women of Color Conference, a university-wide meeting that took place at UCSD. Garza created Black Labin 2018, whose aim is to partner alongside community organizations to support black communities developing strategies. It utilizes surveys that examine topics such as political beliefs, community engagement, experiences of inequality and police violence and abuse, perceptions of civic demonstrations, access to healthcare and economic well-being, the results are also used to identify important policy and regulatory concerns.        </p>
+            Alicia Garza is a co-founder of the Black Lives Matter. Garza has also won the Justice Seekers Glamour Awards. She has helped coordinate the very first 2002 Women of Color Conference, a university-wide meeting that took place at UCSD. Garza created Black Labin 2018, which aims is to partner alongside community organizations to support black communities developing strategies. It utilizes surveys that examine topics such as political beliefs, community engagement, experiences of inequality and police violence and abuse, perceptions of civic demonstrations, access to healthcare and economic well-being, the results are also used to identify important policy and regulatory concerns.        </p>
       </div>
     ),
   },
@@ -43,7 +44,7 @@ const activists = [
     content: (
       <div>
         <p>
-            DeRay Mckesson was an early member of the Black Lives Matter Movement who was involved in Ferguson, Missouri, and Baltimore, Maryland marches and social networking sites including Twitter and Instagram. Along with Johnetta Elzie, Brittany Packnett, and Samuel Sinyangwe, Mckesson introduced Campaign Zero, an advocacy forum to stop excessive brutality. A ten-point action agenda to strengthen policing. Main issues include the decriminalization of trespassing, distribution of drugs, loitering, public nuisance, and drinking of beverages in public as such offenses do not endanger public protection but are used to intimidate African Americans. In July 2017, Baton Rouge police charged Mckesson and Black Lives Matter, who suffered life-altering injury in an arson assault, alleging that Black Lives Matter “incited the violence against police in retaliation for the death of black men shot by police”.         
+            DeRay Mckesson was an early member of the Black Lives Matter Movement who was involved in the Ferguson, Missouri, and Baltimore, Maryland marches and social networking sites including Twitter and Instagram. Along with Johnetta Elzie, Brittany Packnett, and Samuel Sinyangwe, Mckesson introduced Campaign Zero, an advocacy forum to stop excessive brutality. Some main issues covered by Campaign Zero include the decriminalization of trespassing, distribution of drugs, loitering, public nuisance, and drinking of beverages in public as such offenses do not endanger public protection but are used to intimidate African Americans. In July 2017, Baton Rouge police charged Mckesson, who suffered life-altering injury in an arson assault, alleging that Black Lives Matter “incited the violence against police in retaliation for the death of black men shot by police”.         
         </p>
       </div>
     ),
@@ -54,7 +55,7 @@ const activists = [
     content: (
       <div>
         <p>
-            Brittany Packnett is the co-founder of Campaign Zero and co-host of Pod Save the People, an American activist show. She was a part of Obama’s 21s Century Policing Task Force, under President Barack Obama’s presidency. She was Teach for America’s executive director in St. Louis, she was active in the rallies that occurred when a police officer murdered young Michael Brown, and used Facebook and other social outlets to push back against the false image that the media created about the rallies. Packnett was a prominent Black Twitter icon, where she spoke about employment, civil rights, and fair pay. In an appearance on MSNBC in May 2020 regarding Ahmaud Arbery’s death, Oacknett said, “America needs to ask itself why only a viral consumption of black suffering can actually bring action”.        </p>
+            Brittany Packnett is the co-founder of Campaign Zero and co-host of Pod Save the People, an American activist show. She was a part of Obama’s 21st Century Policing Task Force, under President Barack Obama’s presidency. She was Teach for America’s executive director in St. Louis, she was active in the rallies that occurred when a police officer murdered young Michael Brown, and used Facebook and other social outlets to push back against the false image that the media created about the rallies. Packnett was a prominent Black Twitter icon, where she spoke about employment, civil rights, and fair pay. In an appearance on MSNBC in May 2020 regarding Ahmaud Arbery’s death, Oacknett said, “America needs to ask itself why only a viral consumption of black suffering can actually bring action”.        </p>
       </div>
     ),
   },
@@ -64,7 +65,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Johnetta Elzie is one of the leaders in the activist group We The Protestors and co-edits the Ferguson protest newsletter This Is The Movement with fellow activist DeRay Mckesson. She became involved with activism following the shooting of Michael Brown. In January 2015 The Atlantic named her one of the leaders of the Black Lives Matter Movement and The Los Angeles Times has named Elzie to its list of “The new civil rights leaders: Emerging voices in the 21st century”. Elzie has also been a  field organizer for Amnesty International, and has volunteered with a girls group called the Sophia Project in St. Louis.         </p>
+        Johnetta Elzie is one of the leaders in the activist group We The Protestors and co-edits the Ferguson protest newsletter This Is The Movement with fellow activist DeRay Mckesson. She became involved with activism following the shooting of Michael Brown. In January 2015 The Atlantic named her one of the leaders of the Black Lives Matter Movement and The Los Angeles Times has named Elzie to its list of “The new civil rights leaders: Emerging voices in the 21st Century”. Elzie has also been a  field organizer for Amnesty International, and has volunteered with a girls group called the Sophia Project in St. Louis.         </p>
       </div>
     ),
   },
@@ -74,7 +75,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Samuel Sinyanwe is a multicultural protester and American political analyst. He is also a leader of the Black Lives Matter Movement and a co-founder of We the Protesters, a collection of multimedia platforms for Mapping Police Violence. Sinyanwe is a co-host of Pod Save the People show, a show featuring a group of other progressives debating the week’s coverage. Sinyangwe also works as a data scientist with OutStates.org, (a state legislature-oriented organization) and created the Resistance Manual with Mckesson and Brittney Packnett, an open-source initiative intended to link anti-racists with activists focusing on interesting topics.         </p>
+        Samuel Sinyanwe is a multicultural protester and American political analyst. He is also a leader of the Black Lives Matter Movement and a co-founder of We the Protesters, a collection of multimedia platforms to map police violence. Sinyanwe is a co-host of Pod Save the People show, a show featuring a group of other progressives debating the week’s coverage. Sinyangwe also works as a data scientist with OutStates.org (a state legislature-oriented organization) and created the Resistance Manual with Mckesson and Brittney Packnett, an open-source initiative intended to link anti-racists with activists focusing on interesting topics.         </p>
       </div>
     ),
   },
@@ -84,7 +85,7 @@ const activists = [
     content: (
       <div>
         <p>
-            Marissa Johnson is a member of Outside Agitators 206, an advocacy organization founded in Seattle that was dissolved after she was co-chair of Black Lives Matter’s Seattle branch during September 2015. She orchestrated a die-in in November 2013 at a shopping mall in Downtown Seattle which shut down the mall on Black Friday. In January 2015 city council meeting to address the city’s police’s usage of body cameras, she prompted the meeting to be postponed and said “I don't need a home video of my oppression”. In December 2015, Seattle political analyst Maria Tomchich described Johnson as an integral part of the “biggest news story of 2015”, the extension of Black Lives Matter campaign onto the mainstream political landscape, as a consequence of the interruption of Bernie Sanders’ speech.         </p>
+            Marissa Johnson is a member of Outside Agitators 206, an advocacy organization founded in Seattle that was dissolved after she was co-chair of Black Lives Matter’s Seattle branch during September 2015. In December 2015, Seattle political analyst Maria Tomchich described Johnson as an integral part of the “biggest news story of 2015”, the extension of Black Lives Matter campaign onto the mainstream political landscape, as a consequence of the interruption of Bernie Sanders’ speech.         </p>
       </div>
     ),
   },
@@ -94,7 +95,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Bree Newsome is a producer, artist, writer and protester. She is well recognized for her act of protest on June 27,2015, when she was charged in the wake of the Charleston Shooting for taking the Confederate flag from South Carolina state house grounds. The ensuing outrage forced state authorities to drop the flag, and on July 10, 2015, it was officially taken down. She gave interviews to publications and magazines like The Crisis, Vox and Marie Claire, made statements on tv, and delivered public remarks at universities and other venues.         
+        Bree Newsome is a producer, artist, writer and protester. She is well recognized for her act of protest on June 27, 2015, when she was charged in the wake of the Charleston Shooting for taking the Confederate flag from South Carolina state house grounds. The ensuing outrage forced state authorities to drop the flag, and on July 10, 2015, it was officially taken down. She gave interviews to publications and magazines like The Crisis, Vox and Marie Claire, made statements on tv, and delivered public remarks at universities and other venues.         
         </p>
       </div>
     ),
@@ -105,7 +106,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Tamika Mallory is one of the 2017 Women’s March’s main promoters, for which she and her three other co-chairs were honored at that year’s Time 100. Mallory is a supporter of weapons control, feminism, and the activism surrounding Black Lives Matter. At 11, she started her interest in politics by becoming a part of the NAN, a community advocacy network, to know more about the civil rights movement and in 2011 she became the youngest executive director at the NAN. she helped develop the NYC Crisis Management System in 2014, an approved gun violence reduction initiative that provides $20 million annually to groups seeking to combat gun abuse. On January 21, 2017, Mallory, Bob Bland, Carmen Perez, and Linda Sarsour led the 2017 Women’s March rally around the globe. The march was a demonstration against the U.S. inauguration. President Donald Trump, as well as promoting women’s empowerment, legal reform, LGBTIA+ rights, educational care, economic change, social inequality, and racial justice.         </p>
+        Tamika Mallory is one of the 2017 Women’s March’s main promoters, for which she and her three other co-chairs were honored at that year’s Time 100. Mallory is a supporter of weapons control, feminism, and the activism surrounding Black Lives Matter. At 11, she started her interest in politics by becoming a part of NAN, a community advocacy network to know more about the civil rights movement and in 2011 she became the youngest executive director at the NAN. she helped develop the NYC Crisis Management System in 2014, an approved gun violence reduction initiative that provides $20 million annually to groups seeking to combat gun abuse. On January 21, 2017, Mallory, Bob Bland, Carmen Perez, and Linda Sarsour led the 2017 Women’s March rally around the globe. The march was a demonstration against President Donald Trump's U.S. inauguration, and promoted women’s empowerment, legal reform, LGBTIA+ rights, educational care, economic change, social inequality, and racial justice.         </p>
       </div>
     ),
   },
@@ -115,7 +116,7 @@ const activists = [
     content: (
       <div>
         <p>
-            Rashad Robinson is a pioneer in human rights and a charity founder. He serves as president of Color of Change, entering the association in May 2011 and acting as a board member for RaveForward, Demos, State Voices and the Hazen Foundation. Under Robinson’s leadership, Color of Change rose by one million people, growing to four branches in New York, Washington D.C., Los Angeles, adn Oakland, California, and rapidly promoting their global social justice initiatives.         </p>
+            Rashad Robinson is a pioneer in human rights and a charity founder. He serves as president of Color of Change, entering the association in May 2011 and acting as a board member for RaveForward, Demos, State Voices and the Hazen Foundation. Under Robinson’s leadership, Color of Change rose by one million people, growing to four branches in New York, Washington D.C., Los Angeles, and Oakland, California.         </p>
       </div>
     ),
   },
@@ -125,9 +126,20 @@ const activists = [
     content: (
       <div>
         <p>
-            Linda Sarsour was co-chair of the 2017 Women’s March, the 2017 Day Without a Woman, her co-chairs of the Women’s March were interviewed in the 2017 Time Magazine’s “100 Most Influential People”. Sarsour initially received notoriety for opposing American Muslims police repression, subsequently getting active with many human rights concerns such as racial abuse, racism, foreign reform, and mass incarceration. She has led protests for Black Lives Matter and was the sole plaintiff in a case opposing the Trump immigration ban’s legitimacy. In 2012 the White House honored Sarsour as a Champion of Change under Barack Obama’s administration. Sarouer helped lead demonstrations over Black Lives Matter in 2014. Sarsour helped create “Muslims for Ferguson,” and she traveled with other demonstrators to Ferguson, and has since continued to work widely with BLM.         </p>
+            Linda Sarsour was co-chair of the 2017 Women’s March, the 2017 Day Without a Woman. Sarsour initially received notoriety for opposing American Muslims police repression, subsequently getting active with many human rights concerns such as racial abuse, racism, foreign reform, and mass incarceration. She has led protests for Black Lives Matter and was the sole plaintiff in a case opposing the Trump immigration ban’s legitimacy. In 2012 the White House honored Sarsour as a Champion of Change under Barack Obama’s administration. Sarouer helped lead demonstrations over Black Lives Matter in 2014. Sarsour helped create “Muslims for Ferguson,” and she traveled with other demonstrators to Ferguson, and has since continued to work widely with BLM.         </p>
       </div>
     ),
+  },
+  {
+    header: 'Angela Davis',
+    image: angela,
+    content: (
+      <div>
+        <p>
+          Angela Davis is an American political activist, professor, philosopher, and journalist. Davis is a founding member of the Committees of Correspondence for Democracy and Socialism. Davis was active in various movements, such as the second-wave feminist movement, the Black Panther Party, and anti-Vietnam War campaigns. Most of her research focused on abolishing prisons and she co-founded Critical Resistance, a group working to dismantle the prison-industrial complex. Davis was an honorary co-chair of the Women's March in Washington, January 21, 2017, which happened the day after the inauguration of President Trump. Davis was active in the second-wave feminist movement, the Black Panther Party, and anti-Vietnam War campaigns. She co-founded Critical Resistance, a group working to dismantle the prison-industrial complex. Davis was an honorary co-chair of the Women’s March in Washington. 
+        </p>
+      </div>
+    )
   },
   {
     header: 'Marshawn McCarrel',
@@ -135,7 +147,7 @@ const activists = [
     content: (
       <div>
         <p>
-        Marshawn McCarrel was a well-known protester for Black Lives Matter. Pursuing Our Dreams was created by McCarrel, an organization that coordinated weekly “ feed the streets” gatherings to include  prepared lunches for homeless people. He was one of the activists who helped to organize a massive, multi-organization rally named “Freedom Summer” in 2014, plans that were eventually abandoned when a white policeman shot and killed Michael Brown.         </p>
+        Marshawn McCarrel was a well-known protester for Black Lives Matter. McCarrel created Pursuing Our Dreams, an organization that coordinated weekly “ feed the streets” gatherings to include prepared lunches for homeless people. He was one of the activists who helped to organize a massive, multi-organization rally named “Freedom Summer” in 2014, plans that were eventually abandoned when a white policeman shot and killed Michael Brown.         </p>
       </div>
     ),
   },
